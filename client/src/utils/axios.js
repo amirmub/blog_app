af6753d864477369454cc8cdb05305099e959c7d
amirmub <amirmubarek01@gmail.com>
2026-01-16 16:53:53 +0300

    Update BASE_URL to use production endpoint

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const BASE_URL = "http://localhost:4000";
+export const BASE_URL = "https://blog-app-bauq.onrender.com";
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
